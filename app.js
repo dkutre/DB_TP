95afb7f99f7072d39511f18a13e912f4abb3584c
valera <noking007@mail.ru>
2016-10-09 18:15:50 +0300

    добавлена структура роутеров

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+var userRouter   = require('./routers/userRouter');
+var threadRouter = require('./routers/threadRouter');
+var forumRouter  = require('./routers/forumRouter');
+var postRouter   = require('./routers/postRouter');
+
+DATA_BASE_URL = '/db/api/';
+
+
+app.use('/user'   + DATA_BASE_URL, userRouter);
+app.use('/thread' + DATA_BASE_URL, threadRouter);
+app.use('/forum'  + DATA_BASE_URL, forumRouter);
+app.use('/post'   + DATA_BASE_URL, postRouter);
+
 
 app.listen(3000, function () {
   console.log('myApp listening on port 3000!');
