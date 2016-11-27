c98980bf40e76380b7dcf161c832e08b1e4f1694
valera <noking007@mail.ru>
2016-11-27 23:48:49 +0300

    indexs

--- myDb_conf/create_table.sql
+++ myDb_conf/create_table.sql
@@ -1,83 +1,82 @@
-DROP TABLE IF EXISTS subscribers;
-DROP TABLE IF EXISTS posts;
-DROP TABLE IF EXISTS threads;
-DROP TABLE IF EXISTS followers;
-DROP TABLE IF EXISTS forums;
-DROP TABLE IF EXISTS users;
+CREATE DATABASE `myDataBase` 
+USE `myDataBase`;
 
-CREATE TABLE users (
-  id       INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  name     VARCHAR(50),
-  username VARCHAR(50),
-  email    VARCHAR(50) NOT NULL  DEFAULT '',
-  about    TEXT,
-  isAnonymous BOOLEAN NOT NULL DEFAULT 0,
-  UNIQUE KEY(email)
-)DEFAULT CHARSET=utf8;
-/*=================================================*/
-CREATE TABLE forums (
-  id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  name        VARCHAR(50) NOT NULL DEFAULT '' UNIQUE KEY,
-  short_name  VARCHAR(50) NOT NULL DEFAULT '' UNIQUE KEY,
-  user        VARCHAR(50) NOT NULL DEFAULT ''
-)DEFAULT CHARSET=utf8;
-/*=================================================*/
-CREATE TABLE threads (
-    id                 INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    forumShortName     VARCHAR(50) UNIQUE KEY,
-    user               VARCHAR(50) NOT NULL DEFAULT '',
-    title              VARCHAR(50) NOT NULL DEFAULT '',
-    slug               VARCHAR(50) NOT NULL DEFAULT '',
-    message            TEXT NOT NULL DEFAULT '',
-    date               VARCHAR(20) NOT NULL DEFAULT '',
-    likes              INT NOT NULL DEFAULT 0,
-    dislikes           INT NOT NULL DEFAULT 0,
-    isClosed           BOOLEAN NOT NULL DEFAULT 0,
-    isDeleted          BOOLEAN NOT NULL DEFAULT 0,   
-    FOREIGN KEY (user) REFERENCES users(email)
-    ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (forumShortName) REFERENCES forums(short_name)
-    ON DELETE CASCADE ON UPDATE CASCADE
-)DEFAULT CHARSET=utf8;
-/*=================================================*/
-CREATE TABLE  posts (
-    id             INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    userEmail      VARCHAR(50) NOT NULL DEFAULT '',
-    message        TEXT NOT NULL DEFAULT '',
-    forumShortName VARCHAR(50) DEFAULT '',
-    thread_id      INT UNSIGNED NOT NULL,
-    parent         INT NULL DEFAULT NULL,
-    date           VARCHAR(20) NOT NULL DEFAULT '',
-    likes          INT NOT NULL DEFAULT 0,
-    dislikes       INT NOT NULL DEFAULT 0,
-    isApproved     BOOLEAN NOT NULL DEFAULT 0,
-    isHighlighted  BOOLEAN NOT NULL DEFAULT 0,
-    isEdited       BOOLEAN NOT NULL DEFAULT 0,
-    isSpam         BOOLEAN NOT NULL DEFAULT 0,
-    isDeleted      BOOLEAN NOT NULL DEFAULT 0,
-    path           TEXT NOT NULL DEFAULT '', 
-    FOREIGN KEY (userEmail) REFERENCES users(email)
-    ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (forumShortName) REFERENCES forums(short_name)
-    ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (thread_id) REFERENCES threads(id)
-    ON DELETE CASCADE ON UPDATE CASCADE
-)DEFAULT CHARSET=utf8;
-/*=================================================*/
-CREATE TABLE followers (
-    follower_email VARCHAR(50) NOT NULL  DEFAULT '',
-    followee_email VARCHAR(50) NOT NULL  DEFAULT '',
-    UNIQUE (followee_email, follower_email),
-    FOREIGN KEY (follower_email) REFERENCES users(email),
-    FOREIGN KEY (followee_email) REFERENCES users(email)
-) DEFAULT CHARSET=utf8;
-/*=================================================*/
-CREATE TABLE subscribers (
-    user_email VARCHAR(50) NOT NULL,
-    thread_id  INT UNSIGNED  NOT NULL,
-    UNIQUE (user_email, thread_id),
-    FOREIGN KEY (user_email) REFERENCES users(email)
-    ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (thread_id) REFERENCES threads(id)
-) DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `followers`;
+CREATE TABLE `followers` (
+  `followerEmail` varchar(255) NOT NULL,
+  `followeeEmail` varchar(255) NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `forum`;
+CREATE TABLE `forum` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `shortname` varchar(255) NOT NULL,
+  `userEmail` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `shortname_UNIQUE` (`shortname`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;;
+
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE `post` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `isApproved` tinyint(1) NOT NULL DEFAULT '0',
+  `isDeleted` tinyint(1) NOT NULL DEFAULT '0',
+  `isEdited` tinyint(1) NOT NULL DEFAULT '0',
+  `isHighlighted` tinyint(1) NOT NULL DEFAULT '1',
+  `isSpam` tinyint(1) NOT NULL DEFAULT '0',
+  `likes` int(11) NOT NULL DEFAULT '0',
+  `message` varchar(255) NOT NULL,
+  `parent` varchar(255) NOT NULL,
+  `points` int(11) NOT NULL DEFAULT '0',
+  `threadId` int(10) unsigned NOT NULL,
+  `date` datetime NOT NULL,
+  `forumShortname` varchar(255) NOT NULL,
+  `userEmail` varchar(255) NOT NULL,
+  `dislikes` int(11) NOT NULL DEFAULT '0',
+  `materPath` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `subscribes`;
+CREATE TABLE `subscribes` (
+  `userEmail` varchar(255) NOT NULL,
+  `threadId` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `thread`;
+CREATE TABLE `thread` (
+  `date` datetime NOT NULL,
+  `dislikes` int(11) NOT NULL DEFAULT '0',
+  `forumShortname` varchar(255) NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `isClosed` tinyint(1) NOT NULL,
+  `isDeleted` tinyint(1) NOT NULL,
+  `likes` int(11) NOT NULL DEFAULT '0',
+  `message` varchar(5000) NOT NULL,
+  `points` int(11) NOT NULL DEFAULT '0',
+  `posts` int(10) unsigned NOT NULL DEFAULT '0',
+  `slug` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `userEmail` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `email` varchar(255) NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `about` varchar(5000) NOT NULL,
+  `isAnonymous` tinyint(1) NOT NULL DEFAULT '0',
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
